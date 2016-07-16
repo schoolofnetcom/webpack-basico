ca8d52201c6c3ea2f5eda3ecf4afa6f59abc3517
Luiz <argentinaluiz@gmail.com>
2016-07-15 23:37:13 -0300

    Primeiro commit

---
+++ app/firebase-config.js
@@ -0,0 +1,8 @@
+define('firebase-config', function(){
+    return {
+        apiKey: "AIzaSyCpObA1C8_CiPb_z3wSEab5FIn_RkBnmmU",
+        authDomain: "vuejs-firebase-2fe23.firebaseapp.com",
+        databaseURL: "https://vuejs-firebase-2fe23.firebaseio.com",
+        storageBucket: "vuejs-firebase-2fe23.appspot.com",
+    };
+});
